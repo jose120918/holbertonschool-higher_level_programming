fbdebfd27e66797fa6b477e170eb9fbc373b3b02
jose120918 <jose.esteban0918@gmail.com>
2020-12-10 22:33:12 -0500

    Fixed condicionales

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 import sys
 
-print(len(sys.argv) - 1, "arguments:")
+argumentos = sys.argv
+tamaño = len(argumentos) - 1
 
-if __name__ == '__main__':
-    for idx, arg in enumerate(sys.argv):
-       print("{:d}: {}".format((idx + 1), arg))
+if tamaño > 1:
+    print(len(sys.argv), "arguments:")
+    if __name__ == '__main__':
+        for iterador, argumento in enumerate(sys.argv):
+            print("{}: {}".format((iterador + 1), argumento))
+
+elif tamaño == 0:
+        print("{} arguments.".format(tamaño))
+
+else:
+        print("{} argument:".format(tamaño))
+        print("{}: {}".format(tamaño, arg[1]))
