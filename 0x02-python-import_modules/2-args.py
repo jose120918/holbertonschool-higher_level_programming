9314745994a9bea0bf87a15fb55c65aed5a29849
jose120918 <jose.esteban0918@gmail.com>
2020-12-10 22:11:39 -0500

    Try again

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import sys 
-  
-print(len(sys.argv),"arguments:") 
+import sys
+
+print(len(sys.argv) - 1, "arguments:")
 
 if __name__ == '__main__':
     for idx, arg in enumerate(sys.argv):
-       print((idx + 1),":", arg)
+       print("{:d}: {}".format((idx + 1), arg))
