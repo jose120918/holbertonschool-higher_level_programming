933f141352862398df2c7616be80a2fba26f669b
jose120918 <jose.esteban0918@gmail.com>
2020-12-15 19:28:39 -0500

    Print Reversed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in reversed(my_list):
-        print("{:d}".format(i))
+    longitud = len(my_list) - 1
+
+    while longitud != -1:
+        print("{:d}".format(my_list[longitud]))
+        longitud = longitud - 1
