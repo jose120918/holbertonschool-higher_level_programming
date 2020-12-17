23b506e1478c78061e941df2854d753828635667
jose120918 <jose.esteban0918@gmail.com>
2020-12-17 14:19:00 -0500

    fixed print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for fila in matrix:
+        iterador = 0
         for valor in fila:
-            print("{:d}".format(valor), end = " ")
-        print()
+            iterador = iterador + 1
+            print("{:d}".format(valor), end = "")
+            if iterador != len(fila):
+                print(" ", end = "")
+        print("")
