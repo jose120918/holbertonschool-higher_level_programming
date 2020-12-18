717968d31cecb7ed2baa68e8d8a68a4901fae21d
jose120918 <jose.esteban0918@gmail.com>
2020-12-18 13:05:56 -0500

    Fixed value 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = my_list[:]
+    i = 0
     for cont in my_list:
         if cont % 2:
-            new_list[cont] = True
+            new_list[i] = True
         else:
-            new_list[cont] = False
+            new_list[i] = False
+        i += 1
     return new_list
