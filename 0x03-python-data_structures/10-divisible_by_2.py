b20156f1976c4670c39d5bbbd46b2b7241203c99
jose120918 <jose.esteban0918@gmail.com>
2020-12-18 10:33:47 -0500

    Divisible by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = my_list
+    iterador = -1
+    for cont in my_list:
+        iterador += 1
+        if cont % 2:
+            new_list[iterador] = True
+        else:
+            new_list[iterador] = False
