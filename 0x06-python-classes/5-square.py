ea0c2abbed6553170eff36481bfc725f3d4c905c
jose120918 <jose.esteban0918@gmail.com>
2021-01-10 17:46:21 -0500

    Print #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Creación de clase"""
+
+
+class Square:
+    def __init__(self, size=0):
+         """[Constructor]
+
+        Args:
+            Size.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+         """[Constructor]
+
+        Args:
+            Size.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+         """[Constructor]
+
+        Args:
+            Size.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+         """[Constructor]
+
+        Args:
+            Size.
+        """
+        return self.__size*self.__size
+
+    def my_print(self):
+         """[Constructor]
+
+        Args:
+            Size.
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                for y in range(self.__size):
+                    print("#", end="")
+                print()
