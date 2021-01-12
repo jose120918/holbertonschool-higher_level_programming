92927d8546d287acb7f399018d6a90cdd161eb24
jose120918 <jose.esteban0918@gmail.com>
2021-01-11 21:45:32 -0500

    Fixed conditional perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -43,4 +43,7 @@ class Rectangle:
         return self.__height*self.__width
 
     def perimeter(self):
-        return (2 * (self.__height + self.__width))
+        if (self.__height == 0) and (self.__width == 0):
+            return 0
+        else:
+            return (2 * (self.__height + self.__width))
