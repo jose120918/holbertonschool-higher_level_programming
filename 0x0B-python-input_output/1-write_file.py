b8d5cb0f0b19d5472e1810c41a95dea7587b8127
jose120918 <jose.esteban0918@gmail.com>
2021-01-20 15:46:10 -0500

    Write text in document

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Función leer archivos"""
+"""Función escribir en archivos"""
 
 
 def write_file(filename="", text=""):
-    """Funcion basica para leer texto en UTF8"""
+    """Funcion basica para sobreeescribir texto en UTF8"""
     with open(filename, 'w', encoding='utf8') as filetext:
         filetext.write(text)
         return len(text)
