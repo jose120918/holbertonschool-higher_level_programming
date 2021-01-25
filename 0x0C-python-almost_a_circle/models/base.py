08882d8ba4e1e100ae3ee5e9f70786dbd5d18492
jose120918 <jose.esteban0918@gmail.com>
2021-01-25 11:23:33 -0500

    Base Class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Base class"""
+
+class Base:
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Class constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            self.id = Base.__nb_objects + 1
+            Base.__nb_objects = self.id
