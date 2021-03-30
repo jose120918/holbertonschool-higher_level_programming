4ea95b77c469bd4ba456c6781394564bb524176a
jose120918 <jose.esteban0918@gmail.com>
2021-03-29 23:11:45 -0500

    Javascript

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+const argv = process.argv;
+function secondMax (myArray) {
+  if (myArray.length <= 3) {
+    return (0);
+  }
+
+  myArray = myArray.slice(2).map(n => parseInt(n));
+
+  const max = Math.max(...myArray);
+
+  const arr = myArray.filter(arr => arr < max);
+
+  return (Math.max(...arr));
+}
+console.log(secondMax(argv));
+
+/*
+const args = process.argv;
+let smax = 0;
+if (args.length >= 4) {
+  let mymyArray = args.slice(2, args.length);
+  mymyArray.sort();
+  smax = mymyArray[mymyArray.length - 1];
+  mymyArray = mymyArray.filter(mymyArray => mymyArray < smax);
+  smax = mymyArray[mymyArray.length - 1];
+}
+console.log(smax);
+*/
